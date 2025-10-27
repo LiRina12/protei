cd80b9a63121ac7d2f06c5e1625cdfaf6c8445de
LiRina12 <litvinova.irinka2015@yandex.ru>
2025-10-27 18:02:04 +0300

    Реализация считывания из csv файла

--- test_search_and_reverse.py
+++ test_search_and_reverse.py
@@ -2,44 +2,46 @@ import requests
 from requests.exceptions import HTTPError
 import pytest
 import allure
+import csv
 from API_documentation import check_success_request, search_geokoding, reverse_geokoding
-from input_test_data__from_text_file import load_test_data
+from load_test_data__from_csv_file import load_test_data
 from pathlib import Path
 
 
-class TestSearchGeokoding:
+class Nomination:
     """
-    absolute_path: абсолютный путь к файлу test_data_searche.txt с тестовыми данными;
-    формат тестовых  данных: на каждой отдельной строке "запрос,ожидаемый_результат" > [query, expected]
-    test_data: список пар значений [query, expected],  считываем из файла с помощью функции load_test_data
-    Flag True/False показывает, нужно ли делить query дальше на две части (для search не нужно)
+    родительский класс для классов TestSearchGeokoding и TestReverseGeokoding
     """
-    relative_path = Path('test_data_searche.txt')
-    absolute_path = relative_path.resolve()
-    test_data = load_test_data(absolute_path, False)
-    url_end = "search"
     url_nomination = "https://nominatim.openstreetmap.org/"
-    url = url_nomination + url_end
+
+class TestSearchGeokoding(Nomination):
+    """
+    absolute_path: абсолютный путь к файлу test_data_searche.csv с тестовыми данными;
+    test_data: список пар значений [query, expected],  считываем из файла с помощью функции load_test_data
+    """
+    relative_path = Path('test_data_searche.csv')
+    absolute_path = relative_path.resolve()
+    test_data = load_test_data(absolute_path)
+    url_end = "search"
+    url = Nomination.url_nomination + url_end
 
     @allure.feature("search_geokoding")
     @pytest.mark.parametrize("test_data", test_data)
     def test_search(self, test_data):
         query, expected = test_data
-        with allure.step("Отправка в параметрах запроса query, извлечение lon и lat из response_json"):
+        with allure.step(f"Отправка в параметрах запроса query = {query}, извлечение  lon и lat из response_json"):
             result = search_geokoding(self.url, query)
-        with allure.step("Сравнение полученных lon и lat с ожидаемым результатом"):
-            allure.attach(f"Result: {result} VS Expected: {expected}", name="Assert_details", attachment_type=allure.attachment_type.TEXT)
+        with allure.step(f"Сравнение полученного результата = {result}  с ожидаемым результатом = {expected}"):
             assert result == expected
 
 
-class TestReverseGeokoding:
+class TestReverseGeokoding(Nomination):
 
-    relative_path = Path('test_data_reverse.txt')
+    relative_path = Path('test_data_reverse.csv')
     absolute_path = relative_path.resolve()
-    test_data = load_test_data(absolute_path, True)
+    test_data = load_test_data(absolute_path)
     url_end = "reverse"
-    url_nomination = "https://nominatim.openstreetmap.org/"
-    url = url_nomination + url_end
+    url = Nomination.url_nomination + url_end
 
     @allure.feature("reverse_geokoding")
     @pytest.mark.parametrize("test_data", test_data)
@@ -47,7 +49,5 @@ class TestReverseGeokoding:
         lon, lat, expected = test_data
         with allure.step("Отправка в параметрах запроса lon и  lat, извлечение name из response_json "):
             result = reverse_geokoding(self.url, lon, lat)
-        with allure.step("Сравнение полученного name  с ожидаемым результатом"):
-            allure.attach(f"Result: {result} VS Expected: {expected}", name="Assert_details", attachment_type=allure.attachment_type.TEXT)
+        with allure.step(f"Сравнение полученного name = {result}  с ожидаемым результатом expected = {expected}"):
             assert result == expected
-
